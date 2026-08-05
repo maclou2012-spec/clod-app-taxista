a0f8e6c273b4c45fca7f8f5b0bc69907cb624c02
MonariApps <maclou2012@gmail.com>
2026-08-04 21:02:26 -0600

    Integrate Firebase via FlutterFire CLI

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import 'firebase_options.dart';
 import 'routes/app_router.dart';
 import 'theme/clod_theme.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
